1791142d823abaeec0d14f71dcb8131c15b8e19e
Alberto Costa Neto <alberto@dcomp.ufs.br>
2026-04-12 20:59:49 -0300

    Atualização do algoritmo de inserção

--- 2026-1/pi/codigo/insercao.py
+++ 2026-1/pi/codigo/insercao.py
@@ -1,18 +1,21 @@
-def ordenarInsercao(l): 
-   for i in range(1, len(l)):
-      inserirEmOrdem(i, l)
+def ordenarInsercao(l):
+    # Percorre de 1 até o final da lista
+    for i in range(1, len(l)):
+        valor = l[i]
+        print(f'Valor: {valor}')
+        j = i - 1
+        # Move os elementos maiores que o valor para uma posição adiante
+        while j >= 0 and l[j] > valor:
+            print(f'Copiando {l[j]} da posição {j} para a posição {j + 1}')
+            l[j + 1] = l[j]
+            j -= 1
+            print(l)
+        l[j + 1] = valor
+        print(f'Colocando {valor} na posição {j+1}\n{l}')
+    return l
 
 
-def inserirEmOrdem(i, l):
-   j = i
-   while j > 0 and l[j-1] > l[j]:
-      print(f'Colocando {l[j]} no lugar de {l[j-1]} na posição {j-1}')
-      l[j-1], l[j] = l[j], l[j-1]
-      j -= 1
-      print(l)
-   
-
-lista = [3,1,2,10,4,12,15,5]
+lista = [3, 1, 2, 10, 4, 12, 15, 5]
 print('Inicio:', lista)
 ordenarInsercao(lista)
 print('Fim:', lista)
